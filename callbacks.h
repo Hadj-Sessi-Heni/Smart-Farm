a24f68af9e3c88d93850fb55cd90e51e6e20d7c2
HeniHadjSessi <heni.hadjsessi@esprit.tn>
2021-01-05 21:55:50 +0100

    callbaks.h pour la gestion du troupeau et l'authentification

---
+++ callbacks.h
@@ -0,0 +1,114 @@
+#include <gtk/gtk.h>
+
+
+void
+on_return_A_G_clicked                  (GtkWidget      *objet,
+                                        gpointer         user_data);
+
+void
+on_button_ajoua_clicked                (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_treeview1_row_activated             (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_treeview2_row_activated             (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_button2_clicked                     (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button3_suppAjou_clicked            (GtkWidget      *button,
+                                        gpointer         user_data);
+
+void
+on_button2_moda_clicked                (GtkWidget      *button,
+                                        gpointer         user_data);
+
+void
+on_button1_affia_clicked               (GtkWidget      *button,
+                                        gpointer         user_data);
+
+void
+on_button3_clicked                     (GtkWidget      *objet,
+                                        gpointer         user_data);
+
+void
+on_button1_suppa_clicked               (GtkWidget      *button,
+                                        gpointer         user_data);
+
+void
+on_button4Actualiser_clicked           (GtkWidget      *objet,
+                                        gpointer         user_data);
+
+
+void
+on_button5_clicked                     (GtkWidget       *button,
+                                        gpointer         user_data);
+
+
+void
+on_supprimerH_clicked                  (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_validationH_clicked                 (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_radiobutton1H_toggled               (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_radiobutton2HJ_toggled              (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_checkbutton2gb_toggled              (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_checkbutton1ga_toggled              (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_button9H_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button14CrH_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button13log_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Ajout_aut_clicked                   (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_afficher_tree_aut_clicked           (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_treeview4_auth_row_activated        (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+/*
+void
+on_retH_activate                       (GtkWidget       *objet,
+                                       gpointer         user_data);*/ 
+
+void
+on_retH_clicked                        (GtkWidget       *objet,
+                                        gpointer         user_data);
