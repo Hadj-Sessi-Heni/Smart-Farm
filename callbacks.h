94a6bdd59f3ce9039cd5feeded5f6163b666f7a5
ferdaws99 <73741116+ferdaws99@users.noreply.github.com>
2021-01-06 11:32:17 +0100

    fichier gestion équipement

--- callbacks.h
+++ callbacks.h
@@ -2,104 +2,73 @@
 
 
 void
-on_treeview2_ab_row_activated          (GtkTreeView     *treeview,
-                                        GtkTreePath     *path,
-                                        GtkTreeViewColumn *column,
-                                        gpointer         user_data);
-void
-on_button1_afficher_ch_clicked         (GtkWidget       *objet,
+on_Ajouter_clicked                     (GtkButton       *objet,
                                         gpointer         user_data);
 
 void
-on_button_supprimer_ch_clicked         (GtkWidget       *objet,
+on_valider_clicked                     (GtkButton       *objet,
                                         gpointer         user_data);
 
 void
-on_button_modifier_ch_clicked          (GtkWidget       *objet,
+on_retour_clicked                      (GtkButton       *objet,
                                         gpointer         user_data);
 
 void
-on_button1_checher1_ch_clicked         (GtkWidget       *objet,
-                                        gpointer         user_data);
-
-void
-on_radiobutton1_H_ch_toggled           (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
-
-void
-on_radiobutton2_F_ch_toggled           (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
-
-void
-on_button1_ajouter_clicked             (GtkWidget       *objet,
-                                        gpointer         user_data);
-
-void
-on_checkbutton1_sexe_ch_clicked        (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
-
-void
-on_checkbutton2_nom_ch_clicked         (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
-
-void
-on_checkbutton3_prenom_ch_clicked      (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
-
-void
-on_button4_chercher_ch_clicked         (GtkWidget       *objet,
-                                        gpointer         user_data);
-
-void
-on_radiobutton3_H_ch_toggled           (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
-
-void
-on_radiobutton4_F_ch_toggled           (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
-
-void
-on_button5_modifier_ch_clicked         (GtkWidget       *objet,
-                                        gpointer         user_data);
-
-void
-on_treeview1_ch_row_activated          (GtkTreeView     *treeview,
+on_treeview1_row_activated             (GtkTreeView     *treeview,
                                         GtkTreePath     *path,
                                         GtkTreeViewColumn *column,
                                         gpointer         user_data);
 
+
+
 void
-on_radiobutton2_femme_ch_toggled       (GtkToggleButton *togglebutton,
+on_rechercher_clicked                  (GtkButton       *objet,
                                         gpointer         user_data);
 
 void
-on_radiobutton1_homme_ch_toggled       (GtkToggleButton *togglebutton,
+on_actualiser_clicked                  (GtkButton       *objet,
                                         gpointer         user_data);
 
 void
-on_button1_ch_clicked                  (GtkWidget       *objet,
+on_supprimer_clicked                   (GtkButton       *button,
                                         gpointer         user_data);
 
 void
-on_button1_ajouter_ch_clicked          (GtkWidget       *objet,
+on_cancel_clicked                      (GtkButton       *button,
                                         gpointer         user_data);
 
 void
-on_radiobutton1_ch_toggled             (GtkToggleButton *togglebutton,
+on_ok_clicked                          (GtkButton       *button,
                                         gpointer         user_data);
 
 void
-on_button3_apply_ch_clicked            (GtkWidget       *objet,
+on_confirmer_Modif_clicked             (GtkButton       *objet,
+                                        gpointer         user_data);
+
+
+
+void
+on_modifier_clicked                    (GtkButton       *objet,
                                         gpointer         user_data);
 
 void
-on_radiobutton2_ch_toggled             (GtkToggleButton *togglebutton,
+on_betat_acct_clicked                  (GtkButton       *objet,
                                         gpointer         user_data);
 
 void
-on_button2_valider_ch_clicked          (GtkWidget       *objet,
+on_conf_etat_clicked                   (GtkButton       *objet_graphique,
                                         gpointer         user_data);
 
 void
-on_button4_ch_clicked                  (GtkWidget       *objet,
+on_back_clicked                        (GtkButton       *objet,
+                                        gpointer         user_data);
+
+void
+on_eq_uti_clicked                      (GtkButton       *objet,
+                                        gpointer         user_data);
+
+void
+on_treeview2_row_activated             (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
                                         gpointer         user_data);
